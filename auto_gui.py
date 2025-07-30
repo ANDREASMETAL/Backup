25157e1196eaf1cfa91911d9f389d78f191e155f
Andreas Ribeiro Ferreira <andreas.ribeiro.f@gmail.com>
2025-07-30 08:18:20 -0300

    Add files via upload

---
+++ auto_gui.py
@@ -0,0 +1,45 @@
+import pyautogui as pag
+from time import sleep
+
+#pag.mouseInfo()
+
+pag.press('win')
+pag.write('chrome')
+pag.press('enter')
+pag.moveTo(773,366)
+sleep(2)
+pag.click()
+sleep(2)
+pag.write('https://www.retrogames.cc/arcade-games/street-fighter-zero-2-alpha-960813-brazil.html')
+pag.press('enter')
+sleep(1)
+pag.moveTo(692,689)
+pag.click()
+sleep(3)
+pag.moveTo(773,801)
+pag.click()
+sleep(3)
+pag.moveTo(1206,861)
+pag.click()
+sleep(3)
+pag.moveTo(773,801)
+pag.click()
+sleep(10)
+pag.press('back slash')
+pag.press('enter')
+pag.press('enter')
+sleep(3)
+pag.press('enter')
+
+
+
+
+
+
+
+
+
+
+
+#for n in range(3000000000):
+   # pag.click()
---
+++ lista_atividade.py
@@ -0,0 +1,10 @@
+jogos=[]
+
+contador=1
+limite= int(input('Digite a quantidade de jogos qur você deseja adicionar: '))
+
+while contador <= limite:
+     
+    
+    
+    contador= contador+1
